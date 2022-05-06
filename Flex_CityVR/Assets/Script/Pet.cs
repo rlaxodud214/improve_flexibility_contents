6d18880ba49d9cdc954668678265ac1beb3da2e0
O-Wensu <sojk401@naver.com>
2022-05-06 20:55:56 +0900

    상점UI구현

---
+++ Flex_CityVR/Assets/Script/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pet : MonoBehaviour
+{
+    // 상점 ScrollView
+    public GameObject content;
+    public GameObject petItemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /// 동적 Item 추가///
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate<GameObject>(this.petItemPrefab, content.transform);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
